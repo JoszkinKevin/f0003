4d647bf4f5c31840b2bda2935ced7dee0d304119
JoszkinKevin <joszkin65@gmail.com>
2020-11-05 08:33:36 +0000

    Initial commit

---
+++ f0003a.py
@@ -0,0 +1,3 @@
+név=input("Mi a vezetékneved? ")
+név2=input("Mi a keresztneved? ")
+print("Az ön neve: " , név , név2 + ".")
---
+++ f0003b.py
@@ -0,0 +1,10 @@
+név= input("Mi a vezetékneved? ")
+név2= input("Mi a keresztneved? ")
+év = input("Mikor születtél? ")
+csillagjegy = input("Mi a csillagjegyed? ")
+
+print("Az ön születési éve: " , év + ".")
+
+print("Az ön csillagjegye: " , csillagjegy + ".")
+
+print("Az ön neve: " , név , név2 + ".")
